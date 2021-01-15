5a4e36ae1422140a3f23a198cc7e5a21572e6d9f
leo9722 <pralainleopold@gmail.com>
2021-01-15 08:15:24 -0800

    pas form

--- front/src/Components/CalculForm.js
+++ front/src/Components/CalculForm.js
@@ -6,6 +6,11 @@ const CalculForm = () => {
         <div>
             <form action="http://127.0.0.1:8000/api/resultForm" method="POST">
                 <input type="text" id="test" name="test"></input>
+                <input type="radio" name= "contenance" value="9" checked> 9L <br>
+				<input type="radio" name= "contenance" value="12">12L <br>
+				<input type="radio" name= "contenance" value="15">15L <br>
+				<input type="radio" name= "contenance" value="18">18L 
+
                 <button type="submit">Submit</button>
             </form>
 
